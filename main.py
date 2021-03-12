258142b532d41127a0a82cfff04173708c51e2c7
Yevhenii Korolikhin <yevhenii@devforth.io>
2021-03-12 18:49:48 +0200

    Revert "delete preprocess image"
    This reverts commit 71c34e5d43854e0549bbe5c06913a9f005a4eb2c.

--- main.py
+++ main.py
@@ -44,33 +44,52 @@ def stringToImage(base64_string):
     imgdata = base64.b64decode(base64_string)
     return Image.open(io.BytesIO(imgdata))
 
+def toRGB(image):
+    return cv2.cvtColor(np.array(image), cv2.COLOR_BGR2GRAY)
+
+def preprocess_image(image):
+    img_not_noise = cv2.fastNlMeansDenoising(image, None, 10)
+    img_B_A_W = cv2.threshold(gray, 0, 255,
+		cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    return img_B_A_W
+
+
 
 @app.post('/upload/')
 def upload(request: ImageModel): 
-    image = stringToImage(request.base64)
+    msg = base64.b64decode(request.base64)
+    buf = io.BytesIO(msg)
+    image = Image.open(buf)
+    cv2Img = toRGB(stringToImage(request.base64))
+    processed_img = preprocess_image(cv2Img)
 
-    with PyTessBaseAPI(oem=OEM.LSTM_ONLY) as api:
-        api.SetImage(image)
-        api.Recognize()
-        api.SetVariable("save_blob_choices","T")
-        ri=api.GetIterator()
-        level=RIL.TEXTLINE
-        boxes = api.GetComponentImages(RIL.TEXTLINE, True)
-        text_list = []
-        i = 0
-        for r in iterate_level(ri, level):
-            symbol = r.GetUTF8Text(level)
-            conf = r.Confidence(level)
-            bbox = r.BoundingBoxInternal(level)
-            im = {
-                "text": symbol,
-                "left": bbox[0],
-                "top": bbox[1],
-                "width": bbox[2] - bbox[0],
-                "height": bbox[3] - bbox[1],
-            }
-            text_list.append(im)
-            i += 1
+    filename = "{}.png".format(os.getpid())
+    cv2.imwrite(filename, processed_img)
+    
+    
+
+    # with PyTessBaseAPI(oem=OEM.LSTM_ONLY) as api:
+    #     api.SetImage(image)
+    #     api.Recognize()
+    #     api.SetVariable("save_blob_choices","T")
+    #     ri=api.GetIterator()
+    #     level=RIL.TEXTLINE
+    #     boxes = api.GetComponentImages(RIL.TEXTLINE, True)
+    #     text_list = []
+    #     i = 0
+    #     for r in iterate_level(ri, level):
+    #         symbol = r.GetUTF8Text(level)
+    #         conf = r.Confidence(level)
+    #         bbox = r.BoundingBoxInternal(level)
+    #         im = {
+    #             "text": symbol,
+    #             "left": bbox[0],
+    #             "top": bbox[1],
+    #             "width": bbox[2] - bbox[0],
+    #             "height": bbox[3] - bbox[1],
+    #         }
+    #         text_list.append(im)
+    #         i += 1
     return {
-        "texts": text_list,
+        "texts": [],
     }
